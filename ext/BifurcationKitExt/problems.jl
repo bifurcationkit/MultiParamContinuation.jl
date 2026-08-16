c7afdf6d5b97917d7b7675daf22c90025504ae46
romain veltz <romain.veltz@inria.fr>
2026-08-16 19:58:19 +0200

    ext: generate BK constructors for both problem types from one loop

--- ext/BifurcationKitExt/problems.jl
+++ ext/BifurcationKitExt/problems.jl
@@ -1,39 +1,6 @@
 using ForwardDiff
 record_from_solution_nothing(x, p; k...) = nothing
 
-"""
-$SIGNATURES
-
-Make a manifold problem from a `BifurcationProblem`.
-"""
-function ManifoldProblem_BK(F, u0, par;
-                        check_dim::Bool = true,
-                        record_from_solution = record_from_solution_nothing,
-                        project = nothing,
-                        get_radius = get_radius_default,
-                        get_tangent = nothing,
-                        event_function = event_default,
-                        finalize_solution = finalize_default,
-                        prob_cons = nothing)
-    bifprob = BifurcationProblem(F, u0, par, (@optic _))
-    m = length(BK.residual(bifprob.VF, u0, par))
-    ManifoldProblemBK(bifprob, u0, par;
-                        m,
-                        check_dim,
-                        record_from_solution,
-                        project,
-                        get_radius,
-                        get_tangent,
-                        event_function,
-                        finalize_solution,
-                        prob_cons)
-end
-
-jacobian(pb::ManifoldProblemBK, u, p) = BK.jacobian(pb.VF, u, p)
-BK.residual(pb::ManifoldProblemBK, u, p) = BK.residual(pb.VF, u, p)
-d2F(pb::ManifoldProblemBK, x, p, dx1, dx2) = BK.d2F(pb.VF, x, p, dx1, dx2)
-BK.getlens(::ManifoldProblemBK) = nothing
-
 function correct_guess(cache, options::BK.NewtonPar)
     prob = cache.prob
     sol = BK.solve(prob.VF, BK.Newton(), options)
@@ -43,7 +10,84 @@ function correct_guess(cache, options::BK.NewtonPar)
     return sol.u
 end
 
-##############################################################################################################
+for (M, OP) in ((:ManifoldProblem_BK, :ManifoldProblemBK),
+                (:ManifoldProblemBKMatrixFree, :ManifoldProblemBKMatrixFree))
+
+    @eval begin
+        """
+        $SIGNATURES
+
+        Make a manifold problem from a vector field `F`.
+        """
+        function $M(F, u0::AbstractVector, par;
+                    check_dim::Bool = true,
+                    record_from_solution = record_from_solution_nothing,
+                    project = nothing,
+                    get_radius = get_radius_default,
+                    get_tangent = nothing,
+                    event_function = event_default,
+                    finalize_solution = finalize_default,
+                    prob_cons = nothing)
+            bifprob = BifurcationProblem(F, u0, par, (@optic _))
+            m = length(BK.residual(bifprob.VF, u0, par))
+            _make_manifold_problem($OP, bifprob, u0, par, m;
+                        check_dim, record_from_solution, project, get_radius,
+                        get_tangent, event_function, finalize_solution, prob_cons)
+        end
+
+        """
+        $SIGNATURES
+
+        Make a manifold problem from a `BifurcationProblem` and specifying two parameter axes.
+
+        The jacobian of the composite problem `Z = [u; p1; p2]` can be selected with the
+        `jacobian` keyword. It defaults to `nothing` (jacobian of `prob_bk` for the state
+        block and automatic differentiation for the two parameter blocks). Otherwise pass
+        a `BifurcationKit` jacobian marker, e.g. `BK.AutoDiffDense()`, `BK.AutoDiffMF()`,
+        `BK.MatrixFree()` or `BK.FiniteDifferences()`.
+        """
+        function $M(prob_bk::BK.AbstractBifurcationProblem,
+                    u0::AbstractVector,
+                    lens1,
+                    lens2;
+                    check_dim::Bool = true,
+                    project = nothing,
+                    get_radius = get_radius_default,
+                    get_tangent = nothing,
+                    record_from_solution = record_from_solution_nothing,
+                    event_function = event_default,
+                    finalize_solution = finalize_default,
+                    jacobian = nothing)
+            par = BK.getparams(prob_bk)
+            m = length(BK.residual(prob_bk, prob_bk.u0, par))
+
+            # make a bifurcation problem with two parameters axes
+            pb_composite = BifurcationProblem_2P(prob_bk, lens1, lens2, jacobian)
+            new_u0 = vcat(u0, BK._get(par, lens1), BK._get(par, lens2))
+
+            prob_mpc = BifurcationProblem(pb_composite,
+                            new_u0,
+                            par,
+                            (@optic _);
+                            J = (x, p) -> _jacobian_2P(pb_composite, pb_composite.jacobian, x, p)
+                            )
+            𝒯 = eltype(new_u0)
+            Φ = zeros(𝒯, m+2, 2)
+            wbar = zeros(𝒯, m+2)
+            prob_cons = _A(prob_mpc, Φ, Φ' * wbar, wbar)
+
+            _make_manifold_problem($OP, prob_mpc, new_u0, par, m;
+                        check_dim, record_from_solution, project, get_radius,
+                        get_tangent, event_function, finalize_solution, prob_cons)
+        end
+    end
+end
+
+jacobian(pb::AbstractManifoldProblemBifurcationKit, u, p) = BK.jacobian(pb.VF, u, p)
+BK.residual(pb::AbstractManifoldProblemBifurcationKit, u, p) = BK.residual(pb.VF, u, p)
+d2F(pb::AbstractManifoldProblemBifurcationKit, x, p, dx1, dx2) = BK.d2F(pb.VF, x, p, dx1, dx2)
+BK.getlens(::AbstractManifoldProblemBifurcationKit) = nothing
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 """
 $SIGNATURES
 
@@ -63,15 +107,24 @@ function (pb::BifurcationProblem_2P)(Z, par)
     BK.residual(pb.prob, u, par2)
 end
 
-function jacobian(pb::BifurcationProblem_2P, Z, par)
+# Parameter columns ∂F/∂p₁, ∂F/∂p₂ of the composite jacobian. The point is
+# built with `@set` (through the `BifurcationProblem_2P` wrapper) instead of a `vcat`.
+function _jacobian_param(pb::BifurcationProblem_2P, Z, par)
+    p1, p2 = Z[end-1], Z[end]
+    par2 = BK._set(par, (pb.lens1, pb.lens2), (p1, p2))
+    (ForwardDiff.derivative(z -> pb((@set Z[end-1] = z), par2), p1),
+     ForwardDiff.derivative(z -> pb((@set Z[end] = z),   par2), p2))
+end
+
+# Default: use the jacobian of the underlying BifurcationKit problem for the
+# state block and automatic differentiation for the two parameter blocks.
+function _jacobian_2P(pb::BifurcationProblem_2P, ::Nothing, Z, par)
     u = @view Z[1:end-2]
     p1 = Z[end-1]
     p2 = Z[end]
     par2 = BK._set(par, (pb.lens1, pb.lens2), (p1, p2))
-    # @error (@which BK._get_matrix(BK.jacobian(pb.prob, u, par2)))
-    J0 = (BK.jacobian(pb.prob, u, par2))
-    l1 = ForwardDiff.derivative(z -> pb((@set Z[end-1] = z), par), Z[end-1])
-    l2 = ForwardDiff.derivative(z -> pb((@set Z[end] = z),   par), Z[end])
+    J0 = BK.jacobian(pb.prob, u, par2)
+    l1, l2 = _jacobian_param(pb, Z, par)
     hcat(J0, l1, l2)
 end
 
